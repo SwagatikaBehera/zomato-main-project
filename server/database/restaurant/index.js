9fe5485f0db699cb57a83b3b8a87283c29303638
Swagatika Behera <swagatikabehera2001@gmail.com>
2021-07-27 17:52:46 +0530

    added Auth API

--- server/database/restaurant/index.js
+++ server/database/restaurant/index.js
@@ -1,35 +1,40 @@
 import mongoose from "mongoose";
 
-const RestaurantSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  city: { type: String, required: true },
-  address: { type: String, required: true },
-  mapLocation: { type: String, required: true },
-  cuisine: [String],
-  restaurantTimings: String,
-  contactNumber: Number,
-  website: Number,
-  popularDishes: [String],
-  averageCost: Number,
-  amenties: [String],
-  menuImages: {
-    type: mongoose.Types.ObjectId,
-    ref: "Images",
-  },
-  menu: {
-    type: mongoose.Types.ObjectId,
-    ref: "Menus",
-  },
-  reviews: [
-    {
+const RestaurantSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    city: { type: String, required: true },
+    address: { type: String, required: true },
+    mapLocation: { type: String, required: true },
+    cuisine: [String],
+    restaurantTimings: String,
+    contactNumber: Number,
+    website: Number,
+    popularDishes: [String],
+    averageCost: Number,
+    amenties: [String],
+    menuImages: {
       type: mongoose.Types.ObjectId,
-      ref: "Reviews",
+      ref: "Images",
     },
-  ],
-  photos: {
-    type: mongoose.Types.ObjectId,
-    ref: "Images",
+    menu: {
+      type: mongoose.Types.ObjectId,
+      ref: "Menus",
+    },
+    reviews: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Reviews",
+      },
+    ],
+    photos: {
+      type: mongoose.Types.ObjectId,
+      ref: "Images",
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const RestuarantModel = mongoose.model("Restaurants",RestaurantSchema);
