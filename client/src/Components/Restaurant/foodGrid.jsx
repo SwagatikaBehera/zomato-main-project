058569f13b253f551cf81a763de056bc08a1302f
Swagatika Behera <swagatikabehera2001@gmail.com>
2021-10-13 07:44:58 +0530

    add redux reducer to food grid

--- client/src/Components/Restaurant/foodGrid.jsx
+++ client/src/Components/Restaurant/foodGrid.jsx
@@ -6,7 +6,7 @@ const FoodGrid = (props) => {
     <>
       <div className="w-full h-48 rounded-lg md:hidden">
         <img
-          src={props.image[0]}
+          src={props.images.length && props.images[0].location}
           alt="food"
           className="w-full h-full rounded-lg "
         />
@@ -15,7 +15,7 @@ const FoodGrid = (props) => {
       <div className="hidden md:flex gap-2 w-full lg:h-96 md:h-64">
         <div className="w-11/12 h-full overflow-hidden">
           <img
-            src={props.image[0]}
+            src={props.images.length && props.images[0].location}
             alt="food"
             className="w-full h-full object-cover transform transition duration-700 hover:scale-110 filter hover:brightness-110"
           />
@@ -24,14 +24,14 @@ const FoodGrid = (props) => {
         <div className="flex flex-col w-3/12 h-full gap-2">
           <div className="w-full h-full overflow-hidden">
             <img
-              src={props.image[1]}
+              src={props.images.length >= 1 && props.images[1].location}
               alt="food"
               className="w-full h-full object-cover transform transition duration-700 hover:scale-110 filter hover:brightness-110"
             />
           </div>
           <div className="w-full h-full overflow-hidden">
             <img
-              src={props.image[2]}
+              src={props.images.length >= 2 && props.images[2].location}
               alt="food"
               className="w-full h-full object-cover transform transition duration-700 hover:scale-110 filter hover:brightness-110"
             />
@@ -40,7 +40,10 @@ const FoodGrid = (props) => {
 
         <div className="flex flex-col w-3/12 h-full gap-2">
           <div className="w-full h-full relative">
-            <img src={props.image[3]} className="w-full h-full object-cover" />
+            <img
+              src={props.images.length >= 3 && props.images[3].location}
+              className="w-full h-full object-cover"
+            />
             <div className="absolute inset-0 bg-black opacity-40 w-full h-full" />
             <h3 className="w-full h-full absolute inset-y-2/4 text-white text-center text-lg">
               View Gallery
@@ -48,7 +51,7 @@ const FoodGrid = (props) => {
           </div>
           <div className="w-full h-full relative">
             <img
-              src={props.image[4]}
+              src={props.images.length >= 4 && props.images[4].location}
               alt="food"
               className="w-full h-full object-cover"
             />
--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { AiOutlineStar } from "react-icons/ai";
 import { BsBookmarkPlus } from "react-icons/bs";
 import { RiShareForwardLine, RiDirectionLine } from "react-icons/ri";
+
 
 // Component
 import RestaurantNavbar from "../Components/Navbar/RestaurantNavbar";
@@ -11,26 +14,45 @@ import InfoButtons from "../Components/Restaurant/InfoButtons";
 import TabContainer from "../Components/Restaurant/Tabs";
 import CartContainer from "../Components/Cart/CartContainer";
 
+// Redux action
+import { getSpecificRestaurant } from "../Redux/Reducer/restaurant/restaurant.action";
+import { getImage } from "../Redux/Reducer/Image/Image.action";
+
 const RestaurantLayout = (props) => {
+  const [restaurant, setRestaurant] = useState({
+    image: [],
+    name: "",
+    cuisine: "",
+    address: "",
+  });
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSpecificRestaurant(id)).then((data) => {
+      setRestaurant((prev) => ({
+        ...prev,
+        ...data.payload.restaurant,
+      }));
+
+      dispatch(getImage(data.payload.restaurant.photos)).then((data) =>
+        setRestaurant((prev) => ({ ...prev, ...data.payload.image }))
+      );
+    });
+  }, []);
+
   return (
     <>
       <RestaurantNavbar />
       <div className="container mx-auto px-4 lg:px-52 py-8">
-        <FoodGrid
-          image={[
-            "https://b.zmtcdn.com/data/pictures/7/18921277/9f2f843523d0e8b9ecd9ee9ee32c1c46.jpg",
-            "https://b.zmtcdn.com/data/pictures/6/18921286/7ebb5125f196fff3c0e37899d3575d5f.jpg",
-            "https://b.zmtcdn.com/data/pictures/chains/7/18921277/6ddd491a67928f38fcaedf86e3012bab.jpg",
-            "https://b.zmtcdn.com/data/pictures/6/18921286/ef6e8705254e75852eccc5c999820069.jpeg",
-            "https://b.zmtcdn.com/data/zomaland/a6750d1975cd3139cb91535088efb0c71578913426.jpeg",
-          ]}
-        />
+        <FoodGrid images={restaurant?.image} />
         <FoodInfo
-          title="Burger King"
-          restaurantRating="4.3"
-          deliveryRating="4.4"
-          address="Patia, Bhubaneshwar"
-          cuisine="Burger, Fast Food, Beverages, Dessert"
+          title={restaurant?.name}
+          restaurantRating={restaurant?.rating || 0}
+          deliveryRating={restaurant?.rating || 0}
+          address={restaurant?.address}
+          cuisine={restaurant?.cuisine}
         />
 
         <div className="flex flex-wrap gap-2">
